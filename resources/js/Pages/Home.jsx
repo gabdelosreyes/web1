cc00fc97707233988794a103886602c38faf3f3f
gabdelosreyes <delosreyesgabriellgenesis@gmail.com>
2025-02-12 00:35:10 +0800

    progress bar

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,11 @@
-// import React from 'react';
+import { Link } from "@inertiajs/react";
 
 export default function Home({name}) {
     return (
         <>
             <h1 className="title">Test {name}</h1>
+
+            <Link preserveScroll href="/" className="block title mt-[1000px]">{new Date().toLocaleTimeString()}</Link>
         </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    sleep(2);
     return Inertia::render('Home', ['name'=> 'Gab']);
 });
 // Route::inertia('/','Home', ['name'=> 'Gab']); // Much cleaner route
